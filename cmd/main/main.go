a35118305e73bd2ea81e8f6afba66827478a7ea5
Sanchir01 <emgushovs@mail.ru>
2024-08-03 13:38:26 +0300

    init app

---
+++ cmd/main/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "log/slog"
+
+var (
+	development = "development"
+	production  = "production"
+)
+
+func main() {
+	// init config
+
+	// init logger
+
+	// init app golang
+
+	// app gracefully stop
+	slog.Warn("starting app")
+}
